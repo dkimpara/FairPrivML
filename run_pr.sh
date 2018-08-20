cf7e7fcf271f0e1366b3f8ecf78720075d9824e8
Varun Gupta <gupta1.varun@gmail.com>
2018-08-19 21:06:06 -0400

    Edited for param input

--- run_pr.sh
+++ run_pr.sh
@@ -4,18 +4,18 @@
 ###   variables between runs
 
 # enter command line param args to be used: e.g. -e 100
-params=''
+params="-e 1"
 
 # append parameter values in name for readability: e.g. model_eta100_
-model_name=model_
-result_name=result_
+model_name="model_eta1_"
+result_name="result_eta1_"
 
 
 
 for i in 1 2 3 4 5 6 7 8 9 10
 do
-    python train_pr.py -i adultdtrain-sensitivelast -o $model_name$i $params 2>/dev/null
-    python predict_lr.py -i adultdtest-sensitivelast -o $result_name$i -m $model_name$i 2>/dev/null
+    python train_pr.py -i adultdtrain-sensitivelast -o "$model_name$i" $params 2>/dev/null
+    python predict_lr.py -i adultdtest-sensitivelast -o "$result_name$i" -m $model_name$i 2>/dev/null
 
 done
 
@@ -29,8 +29,8 @@ maxmodel=0
 for i in 1 2 3 4 5 6 7 8 9 10
 do
     #echo "result #$i"
-    python fai_bin_bin.py $result_name$i | grep "### Acc" -A 1
-    s=$(python fai_bin_bin.py result_$i | awk '/Acc/ { getline; print }')
+    python fai_bin_bin.py "$result_name$i" | grep "### Acc" -A 1
+    s=$(python fai_bin_bin.py "$result_name$i" | awk '/Acc/ { getline; print }')
     a=( $s )
     v=${a[5]}
     acc=${v::-1}
@@ -45,13 +45,13 @@ for i in 1 2 3 4 5 6 7 8 9 10
 do
  if [ "$i" -ne "$maxmodel" ]
   then
-   rm $model_name$i
-   rm $result_name$i
+   rm "$model_name$i"
+   rm "$result_name$i"
   fi
 done
 
 
 
 printf "\n\n\n"
-echo "Highest accuracy of $maxacc in $model_name$maxmodel"
+echo "Highest accuracy of $maxacc in "$model_name$maxmodel""
 printf "\n\n\n"
